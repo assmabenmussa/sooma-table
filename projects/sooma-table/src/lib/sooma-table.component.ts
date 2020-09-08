ca06348a2dbefa276a5574d4f0b32e2e1e33ceaa
assmabenmussa <assma.bm@gmail.com>
2020-09-08 16:15:12 +0300

    imported code into library

--- projects/sooma-table/src/lib/sooma-table.component.ts
+++ projects/sooma-table/src/lib/sooma-table.component.ts
@@ -1,26 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'sooma-table',
-  template: `
-    <table>
-      <tr *ngFor="let row of arr">
-        {{row}}
-      </tr>
-    </table>
-    `
+  templateUrl: './sooma-table.component.html'
 })
-export class SoomaTableComponent implements OnInit {
+export class SoomaTableComponent implements OnInit, OnChanges {
 
-  arr = [1, 2 , 3]
+  @Output() function: EventEmitter<any> = new EventEmitter<string>();
+  @Input() headings:Array<any>;
+  @Input() data:Array<any>;
+  @Input() parentSubject:Subject<any>;
 
+  p:number = 1;
+  count:number;
+
+  table_config = {
+    titles: undefined,
+    data: undefined,  
+  }
+  
   constructor() { }
-
-
   ngOnInit() {
-    
+    this.getTable();
   }
 
+  ngOnChanges(){
+    this.getTable();
+  }
+
+  getTable(){
+    this.table_config.titles = this.headings;
+    this.table_config.data = this.data;
+    this.count = this.table_config.data.length;
+    console.log("this.table_config data", this.table_config);
+    this.parentSubject.subscribe(reply => {
+      console.log("reply in child from triggered method in parent:", reply);
+    });
+  }
+
+  passParam(action: string, row:Object) {
+    let custom_object = Object.assign({}, row);
+    const param = {action:action, row:custom_object};
+    this.function.emit(param);
+  }
 
 }
